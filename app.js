f4f37fae3916aed070f8e023d90bb1c7513cc823
Megan Kuo <megan.kuo@live.com>
2022-02-12 17:40:41 -0800

    basic auth0 start

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const express = require('express');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_BASE_URL,
+};
 
 module.exports = function (database) {
   const app = express();
 
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(auth(config));
 
   // serve the react app if request to /
   app.use(express.static(path.join(__dirname, 'build')));
